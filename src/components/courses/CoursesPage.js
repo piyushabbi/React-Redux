f5a2b2616f51f17e7991dca5648db3496ee33652
Piyush Abbi <pabbi@sapient.com>
2017-04-30 16:17:32 +0530

    Added course form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
 
 class CoursesPage extends Component {
+  constructor (props) {
+    super(props);
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+
+    this.state = {
+      course: {
+        title: ''
+      }
+    };
+  }
+
+  onTitleChange (e) {
+    const course = this.state.course;
+    course.title = e.target.value;
+
+    this.setState({
+      course: course
+    });
+  }
+
+  onClickSave (e) {
+    e.preventDefault();
+    console.log(`Saving ... ${this.state.course.title}`);
+  }
+
   render () {
     return (
       <div>
         <h1>Courses Page</h1>
+        <h3>Add a Course:-</h3>
+
+        <input type="text"
+              onChange={this.onTitleChange}
+              value={this.state.course.title} />
+
+        <input type="submit"
+              value="Save"
+              onClick={this.onClickSave} />
       </div>
     );
   }
